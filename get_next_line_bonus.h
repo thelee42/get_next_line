183ff06174ba3e5c83ac7e69a8460cdbe1cb18ad
Thea Lee <thelee@bess-f1r1s14.clusters.42paris.fr>
2024-08-06 19:40:06 +0200

    rendu

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thelee <thelee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 18:23:45 by thelee            #+#    #+#             */
+/*   Updated: 2023/12/26 18:25:29 by thelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+int		ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*add_line(char *s1, char *s2);
+
+char	*read_line(int fd, char *str);
+char	*get_line(char *str);
+char	*next_line(char *str);
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thelee <thelee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 18:24:16 by thelee            #+#    #+#             */
+/*   Updated: 2023/12/26 19:19:56 by thelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s != (char)c)
+	{
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return ((char *)s);
+}
+
+char	*add_line(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*stash;
+
+	if (!s1)
+	{
+		s1 = malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
+	if (!s1)
+		return (NULL);
+	stash = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!stash)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		stash[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		stash[i + j] = s2[j];
+	stash[i + j] = '\0';
+	free(s1);
+	return (stash);
+}
